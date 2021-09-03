cfa4261cd3a4fede40511d1ef8e106b78eb4f2a7
oldpand <stanislavoldpande@gmail.com>
2021-09-03 20:50:27 +0300

    First load

---
+++ Depression/Depression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainsAndDepressions
+{
+    class Depression
+    {
+        public int Size { get; private set; }
+
+        public void Increase()
+        {
+            Size++;
+        }
+        public Depression() { }
+        public Depression(int s)
+        {
+            Size = s;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlainsAndDepressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[][]
+            {
+                new int[] {0,1,0,1,0,0,1},
+                new int[] {0,0,1,1,1},
+                new int[] {1,0,0,1,0,1,0,0,0,0},
+                new int[] {1,0,0,1,0,0,0,0,0,0},
+                new int[] {0,0,1,1,0,0,0},
+                new int[] {1,0,1,1,0,0,1,0}
+            };
+
+            List<Depression> depressions = FindDepressions(arr);
+
+            depressions.ForEach(d => {
+                Console.Write(d.Size+ " ");
+            });
+
+            Console.ReadKey();
+        }
+
+        private static List<Depression> FindDepressions(int[][] arr)
+        {
+            List<Depression> depressions = new List<Depression>();
+
+            //max teoretical length 
+            int maxLength = arr.Max(i => i.Length);
+            //we will detect the process of creating depression
+            bool inProcess = false;
+
+            Depression depression = new Depression();
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    //detect empty place of array
+                    if (arr[j].Length-1 < i)
+                    {
+                        if(inProcess)
+                            depressions.Add(depression);
+                        inProcess = false;
+                        continue;
+                    }
+
+                    if (arr[j][i] == 0)
+                    {
+                        if (inProcess)
+                            depression.Increase();
+                        else
+                            depression = new Depression(1);
+
+                        inProcess = true;
+
+                        if (j == arr.Length - 1)
+                        {
+                            inProcess = false;
+                            depressions.Add(depression);
+                        }
+                    } 
+                    else
+                    {
+                        if (inProcess)
+                        {
+                            inProcess = false;
+                            depressions.Add(depression);
+                        }
+                    }
+                }
+            }
+
+            return depressions.OrderBy(d => d.Size).ToList();
+        }
+    }
+}
